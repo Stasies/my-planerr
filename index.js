40f9e8ba3946519922bb0f050402505b3c5f7383
Anastasia <80560435+Stasies@users.noreply.github.com>
2021-09-28 17:44:54 +0500

    Update index.js

--- index.js
+++ index.js
@@ -5,13 +5,10 @@ const mongoose = require("mongoose");
 const authRoute = require("./auth");
 const userRoute = require("./users");
 const postRoute = require("./posts");
-const categoryRoute = require("./categories");
-const multer = require("multer");
 const path = require("path");
 
 dotenv.config();
 app.use(express.json());
-app.use("/images", express.static(path.join(__dirname, "/images")));
 
 mongoose
   .connect(process.env.MONGO_URL, {
@@ -21,23 +18,8 @@ mongoose
   .then(console.log("Connected to MongoDB"))
   .catch((err) => console.log(err));
 
-const storage = multer.diskStorage({
-  destination: (req, file, cb) => {
-    cb(null, "images");
-  },
-  filename: (req, file, cb) => {
-    cb(null, req.body.name);
-  },
-});
-
-const upload = multer({ storage: storage });
-app.post("/api/upload", upload.single("file"), (req, res) => {
-  res.status(200).json("File has been uploaded");
-});
-
 app.use("/api/auth", authRoute);
 app.use("/api/users", userRoute);
 app.use("/api/posts", postRoute);
-app.use("/api/categories", categoryRoute);
 
 app.listen(process.env.PORT || 5000, () => console.log("Server is running..."));
